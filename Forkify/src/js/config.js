4608992c5363c835e5057baf5a0b7401708c841d
David-Guzman02 <guzman.david8@yahoo.com>
2022-06-17 16:54:22 -0400

    First commit

---
+++ Forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '947a4e2c-3965-449c-b4c2-b7faf86aa959';
+export const MODAL_CLOSE_SEC = 2.5;
